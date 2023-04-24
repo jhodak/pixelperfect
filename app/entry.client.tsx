75df9fd140dc17db702ffd49e07b29ae6ba93298
Hodak <10237065+jhodak@users.noreply.github.com>
2023-04-24 07:51:37 -0400

    fix: cart provider in wrong place

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,13 +1,10 @@
 import { ClientProvider } from '@mantine/remix'
 import { RemixBrowser } from '@remix-run/react'
 import { hydrate } from 'react-dom'
-import { CartContextProvider } from './context/cart'
 
 hydrate(
-  <CartContextProvider>
-    <ClientProvider>
-      <RemixBrowser />
-    </ClientProvider>
-  </CartContextProvider>,
+  <ClientProvider>
+    <RemixBrowser />
+  </ClientProvider>,
   document
 )
